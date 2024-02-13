278dd99ec8efd25ba390ff49c46ac228fc8cadd5
ayush ranjan <asayushranjansinha@gmail.com>
2024-02-13 18:29:01 +0530

    login page layout completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [username, setUsername] = useState("");
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main className="h-screen bg-blue-50 flex items-center justify-center">
+      <div className="h-5/6 w-11/12 max-w-lg border border-red-800 flex flex-col justify-center items-center p-6 rounded-md shadow-lg bg-white">
+        <h1 className="text-center text-xl font-bold mb-4">
+          Welcome to <span className="text-red-800">WordleCup</span> Realtime
+          Chat
+        </h1>
+        <div className="flex flex-col w-full mb-4">
+          <label
+            htmlFor="username"
+            className="text-gray-700 font-bold mb-2 text-center"
+          >
+            Enter Your Username
+          </label>
+          <input
+            id="username"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Enter Chat Room
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
