d1863d5db78b4ae50b8c1702502079577ab7106a
ayush ranjan <asayushranjansinha@gmail.com>
2024-02-14 19:01:46 +0530

    message style change

--- client/src/components/SentMessage.jsx
+++ client/src/components/SentMessage.jsx
@@ -6,9 +6,9 @@ const SentMessage = ({ message, timestamp }) => {
     <div className="flex justify-end">
       <div className="mr-4 w-fit min-w-40 flex items-center bg-green-300 rounded-lg shadow-md mb-4 px-4 py-2">
         {/* Message content */}
-        <div className="flex flex-col ml-6">
-          <p className="mt-1 text-gray-700">{message}</p>
-          <span className="text-gray-500 text-sm mt-2">{timestamp}</span>
+        <div className="w-full flex flex-col">
+        <p className="mt-1 text-gray-700 text-sm">{message}</p>
+          <span className="w-full text-xs text-gray-500 text-end">{timestamp}</span>
         </div>
       </div>
     </div>
